a23829ad501a6d66407dc420499f27b251b062bc
akkalimullina <sept88791@gmail.com>
2025-02-16 21:52:19 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Задача 1
+  let str = "abcde";
+  alert(str[0]);
+  alert(str[1]);
+  alert(str[2]);
+  alert(str[3]);
+  alert(str[4]);
+  // меняя цифры от этого разные буквы
+
+  // Задача 2
+  let a = prompt("Введите первое число:");
+  let b = prompt("Введите второе число:");
+  console.log(a > 0 || b > 0);
+
+  // Задача 3
+  let age = prompt("Введите ваш возраст:");
+  if (age >= 18) {
+    console.log("Совершеннолетний");
+  } else {
+    console.log("Несовершеннолетний");
+  }
+});
